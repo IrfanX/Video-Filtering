44e168a7bf92b64db09e5f16ed29518728c59849
IrfanX <abdulwahabirfan@outlook.com>
2022-01-12 12:14:14 +0800

    Update imagefilter.py

--- imagefilter.py
+++ imagefilter.py
@@ -32,34 +32,6 @@ def makeYellow(frame):
 
 	return cv2.merge([b, g, r])
 
-'''
-#Splitting, Mixing and Mergin
-(b, g, r) = cv2.split(img)
-b = b.astype(float)
-g = g.astype(float)
-r = r.astype(float)
-
-#b *= 1.3
-g *= 1.3
-r *= 1.3
-
-b = b.astype(int)
-g = g.astype(int)
-r = r.astype(int)
-b = np.clip(b, 0, 255)
-g = np.clip(g, 0, 255)
-r = np.clip(r, 0, 255)
-
-print(np.amax(b))
-print(np.amax(g))
-print(np.amax(r))
-
-b = b.astype(np.uint8)
-g = g.astype(np.uint8)
-r = r.astype(np.uint8)
-newImage = cv2.merge([b, g, r])
-'''
-
 newImage = makeYellow(img)
 
 cv2.imshow("Yellow Image", newImage)
